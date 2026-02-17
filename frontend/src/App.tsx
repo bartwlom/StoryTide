914f927055ce2e4d47db57dfd197b8b3ca19299f
abrtwcom <amitbartwal008@gmail.com>
2026-02-17 19:14:23 +0530

    line 14 app.tsx frontend fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 import { Signup } from "./pages/Signup"
-import { Signin } from "./pages/Signin"
+import { Signin } from  "./pages/Signin"
 import { Blog } from './pages/Blog'
 import { Blogs } from './pages/Blogs'
 import { Publish } from './pages/Publish'
