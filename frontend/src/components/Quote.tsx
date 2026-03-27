18050f35c88555326055db4a6e1609f8d97e9194
abrtwcom <amitbartwal008@gmail.com>
2026-03-27 20:22:52 +0530

    msg

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -5,9 +5,12 @@ export const Quote = () => {
                 Quote of the Day
             </div>
             <div className="max-w-md text-xl font-semibold text-left mt-4">
-                "The only way to do great work is to love what you do.If you don't find what you love keep looking and settle as with all matters of heart you'ill know when you find it"
-                "stay hungry stay foolish"
+                "The only way to do great work is to love what you do. If you don't find what you love keep looking and don't settle. As with all matters of the heart, you'll know when you find it."
             </div>
+            <div className="max-w-md text-xl font-semibold text-left mt-2">
+                "Stay hungry, stay foolish."
+            </div>
+
             <div className="max-w-md text-sm font-light text-slate-400">
                 - Steve Jobs
             </div>
